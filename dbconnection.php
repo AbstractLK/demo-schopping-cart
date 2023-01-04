07f756cde5e06417c1a07995bf8f8d6494fcdc5d
Kaveesha <kalshandhanasekara@gmail.com>
2023-01-04 17:30:12 +0530

    refresh

---
+++ dbconnection.php
@@ -0,0 +1,28 @@
+<?php
+// $servername = "localhost";
+// $username = "root";
+// $password = "";
+// $dbname = "shop";
+
+// Create connection
+// $conn = new mysqli($servername, $username, $password, $dbname);
+// // Check connection
+// if ($conn->connect_error) {
+//   die("Connection failed: " . $conn->connect_error);
+// }
+
+function getDbConnection(){
+  $servername = "localhost";
+  $username = "root";
+  $password = "";
+  $dbname = "shop";
+
+    $conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+    return $conn;
+}
+ 
+?>
